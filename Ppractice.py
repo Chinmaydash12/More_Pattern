4d2d87e2519efc68bc1a20393b7ce2ce627d56ed
Chinmaydash12 <78595116+Chinmaydash12@users.noreply.github.com>
2021-02-11 19:27:26 +0530

    Add files via upload

---
+++ Ppractice.py
@@ -0,0 +1,59 @@
+'''age=int(input('Enter age'))
+if age<=18:
+    print('Minor')
+if age>18 and age<60:
+        print('Adult')
+if age>=60:
+            print('Senior')
+if age==54:
+                print('This is also checked')'''
+'''age=int(input('Enter age'))
+if age<=18:
+    print('Minor')
+elif age>18 and age<=60:
+    print('Adult')
+elif age>60 and age<100:
+    print('Senior')
+elif age==54:
+    print('this is')
+else:
+    print('Invalid')'''
+
+        
+'''for x in range(6):
+    if x==3:
+        continue
+    print('Hi')
+    print(x)
+    print('End')'''
+'''for x in range(6):
+    if x==3:
+        break
+    print('Hi')
+    print(x)
+    print('End')'''
+'''for x in range(6):
+    if x==3:
+        pass
+    print('Hi')
+    print(x)
+    print('End')'''
+'''for x in range(1,5):
+    if x%2!=0:
+        print('B',end='')
+    else:
+            print('W',end='')'''
+'''for x in range(1,5):
+            if x==x:
+               print('B',end='')'''
+for x in range(1,5):
+     if x%2!=0:
+         print('B',end='')
+     else:
+        print('W',end='')
+    
+     if x%2!=0:
+         print('B',end='')
+     else:
+        print('W',end='')     
+
